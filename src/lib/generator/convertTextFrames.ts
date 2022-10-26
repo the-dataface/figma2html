d3c7ad724f7f1e9aa418a9cbe1cd1758b79383e8
svickars <sam@thedataface.com>
2022-10-26 16:39:54 -0700

    fix applyHtags + applyStyleNames

--- src/lib/generator/convertTextFrames.ts
+++ src/lib/generator/convertTextFrames.ts
@@ -30,8 +30,8 @@ export default (textFrames, frameWidth, frameHeight) => {
 
 		let segments = textFrame.getStyledTextSegments(props);
 		const styleId = textFrame.textStyleId;
-		// const styleObject = figma.getStyleById(styleId);
-		const styleObject = false;
+		const styleObject = figma.getStyleById(styleId);
+		// const styleObject = false;
 
 		segments.forEach((seg, i) => {
 			// get styles object from included props
@@ -43,21 +43,21 @@ export default (textFrames, frameWidth, frameHeight) => {
 				i === 0
 					? true
 					: stylesObject['font-family'] ===
-							textSegments[0].stylesObject['font-family'] &&
-					  stylesObject['font-size'] ===
-							textSegments[0].stylesObject['font-size'] &&
-					  stylesObject['letter-spacing'] ===
-							textSegments[0].stylesObject['letter-spacing'] &&
-					  stylesObject['color'] ===
-							textSegments[0].stylesObject['color'] &&
-					  stylesObject['line-height'] ===
-							textSegments[0].stylesObject['line-height'] &&
-					  stylesObject['mix-blend-mode'] ===
-							textSegments[0].stylesObject['mix-blend-mode'] &&
-					  stylesObject['text-decoration'] ===
-							textSegments[0].stylesObject['text-decoration'] &&
-					  stylesObject['text-transform'] ===
-							textSegments[0].stylesObject['text-transform'];
+					textSegments[0].stylesObject['font-family'] &&
+					stylesObject['font-size'] ===
+					textSegments[0].stylesObject['font-size'] &&
+					stylesObject['letter-spacing'] ===
+					textSegments[0].stylesObject['letter-spacing'] &&
+					stylesObject['color'] ===
+					textSegments[0].stylesObject['color'] &&
+					stylesObject['line-height'] ===
+					textSegments[0].stylesObject['line-height'] &&
+					stylesObject['mix-blend-mode'] ===
+					textSegments[0].stylesObject['mix-blend-mode'] &&
+					stylesObject['text-decoration'] ===
+					textSegments[0].stylesObject['text-decoration'] &&
+					stylesObject['text-transform'] ===
+					textSegments[0].stylesObject['text-transform'];
 
 			// is this segment's font-weight 700 (bold) (only if isBaseStyle is false)?
 			const isBold = isBaseStyle && stylesObject['font-weight'] === 700;
@@ -65,8 +65,8 @@ export default (textFrames, frameWidth, frameHeight) => {
 			// is this segment's font-weight neither 400 or 700 (only if isBaseStyle is false)?
 			const isOtherWeight =
 				isBaseStyle &&
-				stylesObject['font-weight'] !== 400 &&
-				stylesObject['font-weight'] !== 700
+					stylesObject['font-weight'] !== 400 &&
+					stylesObject['font-weight'] !== 700
 					? stylesObject['font-weight']
 					: false;
 
@@ -92,7 +92,7 @@ export default (textFrames, frameWidth, frameHeight) => {
 		if (styleId && typeof styleId !== 'symbol' && styleObject)
 			elClass += ` ${dashify(
 				styleObject.name.split('/')[
-					styleObject.name.split('/').length - 1
+				styleObject.name.split('/').length - 1
 				]
 			)}`;
 
@@ -100,6 +100,7 @@ export default (textFrames, frameWidth, frameHeight) => {
 		const tag = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'].includes(trim(elClass))
 			? trim(elClass)
 			: 'p';
+
 		const baseStyle = {
 			tag,
 			style: textSegments[0].styleString
