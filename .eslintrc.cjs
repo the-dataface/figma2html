2cb8dc09cb8cd18ce76c0bf05ad3cbd177cc9224
Sawyer Click <src2188@columbia.edu>
2022-12-23 19:22:28 -0600

    [style] eslint + prettier

---
+++ .eslintrc.cjs
@@ -0,0 +1,15 @@
+module.exports = {
+	root: true,
+	extends: ['eslint:recommended', 'prettier'],
+	plugins: ['svelte3'],
+	overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
+	parserOptions: {
+		sourceType: 'module',
+		ecmaVersion: 'latest',
+	},
+	env: {
+		browser: true,
+		es2017: true,
+		node: true,
+	},
+};
